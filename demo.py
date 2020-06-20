695261597c250c928dc5e200bdfe575aa749f75c
evjeny <gkanafing@gmail.com>
2020-06-20 17:13:47 +0500

    add per day graphs

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ import matplotlib.cm as cmx
 
 import pickle
 import joblib
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 
 @st.cache
@@ -20,7 +20,6 @@ def read_test_data():
     return x, y
 
 
-@st.cache
 def load_models():
     knn = joblib.load("bins/cluster_knn.joblib")
     segment_binarizer = joblib.load("bins/segment_binarizer.joblib")
@@ -32,10 +31,15 @@ def load_models():
 def display_graph(x, segment_binarizer):
     fig, ax = plt.subplots(1, figsize=(10, 5))
 
-    x = [5]
+    ax.set_title("Граф покупок пользователя")
+    ax.set_xlabel("Время в днях")
+
+    x_data = [0]
     for time_delta in x[:-1, -1]:
-        x.append(x[-1] + time_delta)
-    y = [10] * len(x)
+        x_data.append(x_data[-1] + time_delta)
+    y_data = [10] * len(x_data)
+
+    ax.plot([x_data[0], x_data[-1]], [y_data[0], y_data[-1]], c="black", zorder=0)
 
     nodes = segment_binarizer.inverse_transform(x[:, :-1]).reshape(-1)
 
@@ -48,24 +52,49 @@ def display_graph(x, segment_binarizer):
     for color in range(a, b+1):
         color_mapping[color] = scalarMap.to_rgba(color)
     
-    y_colors = [color_mapping[y_i] for y_i in y]
+    y_colors = [color_mapping[node] for node in nodes]
 
-    ax.scatter(x, y, c=y_colors)
+    ax.scatter(x_data, y_data, c=y_colors, s=80, zorder=1)
 
-    return ax
+    return fig
+
+
+def data_in_n_days(x, n, segment_net):
+    x_batch = x.reshape(1, x.shape[0], x.shape[1])
+    x_batch[-1, -1] = n
+
+    prediction = segment_net.predict(x_batch).reshape(-1)
+
+    fig, ax = plt.subplots(1, figsize=(8, 4))
+    ax.set_title("Вероятности перехода в новые сегменты")
+    ax.set_xlabel("id сегмента")
+    ax.set_ylabel("вероятность")
+    ax.set_ylim(-0.05, 1.05)
+
+    ax.bar(range(len(prediction)), prediction)
+
+    return fig
 
 
 st.title("Segmentator")
 
-xs, ys = read_test_data()
-st.write(len(ys), "samples in total")
+knn, segment_binarizer, segment_net = load_models()
 
-index = st.slider("Inex to analyze", 0, len(xs)-1)
+xs, ys = read_test_data()
+st.write("Загружено", len(ys), "семплов")
+
+index = st.slider("Какого пользователя анализировать", 0, len(xs)-1)
 
 x_cur = xs[index]
 y_cur = ys[index]
 
-knn, segment_binarizer, segment_net = load_models()
-
 st.write(display_graph(x_cur, segment_binarizer))
 
+n = st.slider("Сколько дней анализировать", 1, 40)
+days = [1] + list(range(5, n+1, 5))
+
+st.write("### Прогноз переходов на ближайшие {} дней".format(n))
+
+for cur_days in days:
+    st.write("Через", cur_days, "дней:")
+    st.write(data_in_n_days(x_cur, cur_days, segment_net))
